83e4c037845e68ed244f1857517f50a96fd88d18
Weldyson de Azevedo <41272112+weldyson@users.noreply.github.com>
2026-01-21 11:14:08 -0300

    Initial commit: Spring Boot project setup
    Set up a new Spring Boot project with Maven, including basic configuration files, application entry point, and a test class. Added dependencies for JPA, validation, web, Flyway, PostgreSQL, and Lombok. Configured application properties for PostgreSQL and Flyway.

---
+++ src/main/java/com/example/weldyson/korteSaas/Korte_SaaS/KorteSaaSApplication.java
@@ -0,0 +1,13 @@
+package com.example.weldyson.korteSaas.Korte_SaaS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorteSaaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorteSaaSApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weldyson/korteSaas/Korte_SaaS/KorteSaaSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weldyson.korteSaas.Korte_SaaS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorteSaaSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
